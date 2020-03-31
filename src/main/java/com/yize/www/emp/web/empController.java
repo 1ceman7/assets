df8fde89a0de5f2668a1d8f6f9c45e41a132b4c1
1ceman7 <ice7i@qq.com>
2020-03-31 13:06:40 +0800

    新建empController类

---
+++ src/main/java/com/yize/www/emp/web/empController.java
@@ -0,0 +1,10 @@
+package com.yize.www.emp.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class empController {
+    /*@Autowired
+    private empService empService;*/
+}
