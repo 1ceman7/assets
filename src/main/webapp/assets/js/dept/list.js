f6812d9b1ad5a5cf1af4340883bfff384c59cf62
1ceman7 <ice7i@qq.com>
2020-04-10 13:23:49 +0800

    2020.04.10 13：23完善查询展示

--- src/main/webapp/assets/js/dept/list.js
+++ src/main/webapp/assets/js/dept/list.js
@@ -2,7 +2,9 @@
 var qjbl="";
 var tb = "";
 var tr = "";
+//td1 装span
 var td1 = "";
+//span 装修改和删除按钮
 var span = "";
 var update = "";
 var del = "";
@@ -53,7 +55,7 @@ function getQuery() {
 	$('#query').live('click','a',function(){
 		var queryName = $('#queryName').val();
 		var queryTel = $('#queryTel').val();
-		alert(queryName);
+		alert('输入的名称是：'+queryName);
 		// 清空 tbody 的所有元素
 		//tb = $("#Ttest1");
 		// 清空 tbody 的所有元素
@@ -75,23 +77,11 @@ function getQuery() {
 			for (var i = 0; i < queryList.length; i++){
 				//将id赋值到全局变量里，删除时使用。
 				qjbl=queryList[i].id;
-				var name = queryList[i].name;
-				var natd = $('<td>'+name+'</td>');
-				var tel = queryList[i].tel;
-				var tetd = $('<td>'+tel+'</td>>');
-
-				tr.append(qjbl);
-				tr.append(natd);
-				tr.append(tetd);
-				tb.append(tr);
-				alert('id = '+qjbl+ '电话=' +tel+'名称='+name);
-				return;
-				/*tr = $('<tr align="center" bgcolor="#FFFFFF">');
+				tr = $('<tr align="center" bgcolor="#FFFFFF">');
 				td1 = $('<td width="13%" height="30">');
 				span = $('<span style="line-height:12px; text-align:center;">');
 				update = $('<a href="" class="xiu" id="update">修改&nbsp&nbsp</a>');
 				del = $('<a href="" class="xiu" id="dele" onclick="">删除</a>');
-
 				//将"修改、删除“的按钮添加到span中
 				span.append(update);
 				span.find(update).attr("href","/Test_assets/assets/jsps/department/input.html?id="+queryList[i].id);
@@ -102,7 +92,7 @@ function getQuery() {
 				appendTd(tr,queryList[i],'name');
 				appendTd(tr,queryList[i],'tel');
 				tr.append(td1);
-				tb.append(tr);*/
+				tb.append(tr);
 			}
 		}
 	})
